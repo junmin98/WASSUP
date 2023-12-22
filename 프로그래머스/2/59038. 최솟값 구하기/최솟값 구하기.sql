1c49d4b8ef26cbb6d38d497ff20462fafc92e407
junmin <jxxnmin98@gmail.com>
2023-12-22 17:55:14 +0900

    pandas 231222

--- 프로그래머스/2/59038. 최솟값 구하기/최솟값 구하기.sql
+++
@@ -1,3 +0,0 @@
--- 코드를 입력하세요
-SELECT MIN(DATETIME) AS '시간'
-FROM ANIMAL_INS
--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++
@@ -1,5 +0,0 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE (ANIMAL_TYPE='Dog') AND ((NAME LIKE '%EL') OR (NAME LIKE 'EL%') OR (NAME LIKE '%EL%'))
-ORDER BY NAME; # -> 'Elijah', 'Ella'
